5f63056318992e99ed138c8da315440fca87b49f
hyeji <khji1210@naver.com>
2021-02-25 01:13:48 +0900

    modify : MissingNumbers

--- HackerRank/search/easy/MissingNumbers.java
+++ HackerRank/search/easy/MissingNumbers.java
@@ -1,34 +1,7 @@
 import java.util.*;
 
 public class MissingNumbers {
-    // Solution1
-    static int[] solution1(int[] arr, int[] brr) {
-        Set<Integer> missNums = new HashSet<>();
-        int j = 0;
-        for (int i = 0; i < brr.length; i++) {
-            if (j == arr.length) {
-                missNums.add(brr[i]);
-                continue;
-            }
-            if (arr[j] != brr[i]) {
-                missNums.add(brr[i]);
-                continue;
-            }
-            j++;
-        }
-        
-        int[] result = new int[missNums.size()];
-        int i = 0;
-        for (Integer num : missNums) {
-            result[i] = num;
-            i++;
-        }
-        Arrays.sort(result);
-        return result;
-    }
-
-    // Solution2
-    public int[] solution2(int[] arr, int[] brr) {
+    public int[] solution(int[] arr, int[] brr) {
         TreeMap<Integer, Integer> map = new TreeMap<>();
     
         for (int i : brr) {
@@ -44,7 +17,7 @@ public class MissingNumbers {
     
         int[] result = new int[map.size()];
         int i = 0;
-        for (Entry<Integer, Integer> entry : map.entrySet()) {
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
           result[i++] = entry.getKey();
         }
     
